7b6cd1fbf2cf546dfb44123638a1a46f130c9f13
Apple <apple@test.com>
2024-06-16 01:45:54 +0800

    feat: 初始化配置

---
+++ src/utils/welcomelog.ts
@@ -0,0 +1,17 @@
+import chalk from 'chalk';
+
+console.log(chalk.blue('**************************************************'));
+console.log(chalk.green('               Welcome to My Admin              '));
+console.log(chalk.blue('**************************************************'));
+
+console.log(
+  chalk.yellow(`
+┳┓┏┓┏┓┏┓┏┳┓  ┏┓┳┓┳┳┓┳┳┓
+┣┫┣ ┣┫┃  ┃   ┣┫┃┃┃┃┃┃┃┃
+┛┗┗┛┛┗┗┛ ┻   ┛┗┻┛┛ ┗┻┛┗
+`),
+);
+
+console.log(chalk.blue('**************************************************'));
+console.log(chalk.green('                 Have a Great Day!                '));
+console.log(chalk.blue('**************************************************'));
